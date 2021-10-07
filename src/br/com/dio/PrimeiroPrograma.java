2191544f49e6e6283cd0177d4f71ae9647c43a7f
deyvis0n <deyvisonsferreira@outlook.com>
2021-10-07 15:14:22 -0300

    Utilizando classe livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,6 +1,7 @@
 package br.com.dio;
 
 import br.com.dio.model.Gato;
+import br.com.dio.model.Livro;
 
 public class PrimeiroPrograma {
 
@@ -9,5 +10,9 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
+
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+
+        System.out.println(livro);
     }
 }
